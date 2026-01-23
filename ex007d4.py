6fac9ce72f9de6c7489762b8d533d701a95cb5fa
Riguerson <riguersonsantos899@gmail.com>
2026-01-23 11:23:39 -0300

    commit do desafio 4 aula 7

---
+++ ex007d4.py
@@ -0,0 +1,6 @@
+valorMts = float(input("Digite o valor em metros: "));
+
+valorCtms = valorMts * 100;
+valorMms = valorMts * 1000;
+
+print("{0} metros equivalem há {1} centimetros e {2} milimetros".format(valorMts, valorCtms, valorMms));
