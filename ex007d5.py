b5f7233dd502c589d9a342eaa61f0b874b4b8325
Riguerson <riguersonsantos899@gmail.com>
2026-01-23 11:52:50 -0300

    commit do desafio 5

---
+++ ex007d5.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um numero para saber sua tabuada: "))
+
+tabuada = n*1, n*2, n*3, n*4, n*5, n*6 , n*7, n*8, n*9, n*10;
+
+print("A tabuada do 1 ao 10 de {0} é: \n" 
+     "{1}".format(n, tabuada,))
